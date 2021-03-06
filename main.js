a39e8e830814f63159751fc604bc5cc32d5fdc71
Christino2020 <76167050+Christino2020@users.noreply.github.com>
2021-03-06 18:50:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+
+var First_name= "Prasad";
+console.log(First_name);
+
+var last_name = "Sreedher";
+console.log(last_name);
+
+var Full_name = First_name.concat(last_name);
+console.log(Full_name);
+
+var Name = "My name is Prasad Singh";
+console.log(Name);
+
+var ReplaceName = Name.replace("Singh", "Sreedher");
+console.log(ReplaceName);
+
+var Upper = "CODING" ;
+var Lower = Upper.toLowerCase();
+console.log(Lower);
+
+var Lower2 = "coding";
+var Upper2 = Lower2.toUpperCase();
+console.log(Upper);
+
+var mathsqrt = Math.sqrt(16);
+console.log(mathsqrt);
+
+var mathpow = Math.pow(3, 4);
+console.log(mathpow);
+
+var Lenghtofname =  Full_name.length;
+console.log(Lenghtofname);
+
+var BigNumber = 64885327.53776765347;
+console.log(BigNumber);
+var Fixed = BigNumber.toFixed(3);
+console.log(Fixed)
+
+var maxno = Math.max(53463546537563974698659746.647354862357364873548764, 745743756239574357346573465874.6357635763456375);
+console.log(maxno);
+
+var minno = Math.min(63764756439847230970343865984360328658346508126340.2647354753294863289469759346389469, 627346973257325432754732469237463924629.4634693746983246824684);
+console.log(minno);
+
